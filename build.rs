c0718d5ea4c511a223cf52c0e317fd053d45ce3f
Ben Frederickson <github@benfrederickson.com>
2021-01-14 23:02:01 -0800

    build examples / enable unwind

--- build.rs
+++ build.rs
@@ -7,6 +7,7 @@ fn main() {
         "x86_64" | "arm" => {}
         _ => return
     };
+    let target= env::var("TARGET").unwrap();
 
     match env::var("CARGO_CFG_TARGET_OS").unwrap().as_ref() {
         "linux" => {
@@ -15,6 +16,8 @@ fn main() {
                 println!("cargo:rustc-cfg=use_libunwind");
                 if env::var("CARGO_CFG_TARGET_ENV").unwrap() == "musl" {
                     println!("cargo:rustc-link-search=native=/usr/local/lib");
+                    println!("cargo:rustc-link-search=native=/usr/local/musl/{}/lib", target);
+                    println!("cargo:rustc-link-lib=static=z");
                     println!("cargo:rustc-link-lib=static=unwind");
                     println!("cargo:rustc-link-lib=static=unwind-ptrace");
                     println!("cargo:rustc-link-lib=static=unwind-{}", target_arch);
